49d44ae1bbd467e4dfd5043177097082f9c1e3f3
SergejShk <sshkurupij11@gmail.com>
2022-07-14 23:30:45 +0300

    add api-service trending

--- src/js/main.js
+++ src/js/main.js
@@ -3,22 +3,44 @@ import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import articlesTpl from '../templates/articlesTpl.hbs';
 
 const apiService = new ApiService();
-const galleryListEl = document.querySelector(".gallery__grid")
 
-async function handleQueryApi() {
+const galleryListEl = document.querySelector('.gallery__grid');
+const paginationBtnContainerEl = document.querySelector(
+  '.pagination__container'
+);
+
+const handleQueryApi = async () => {
   try {
     Loading.circle('Loading...');
     const data = await apiService.getTrendingArticles();
 
-    appendArticlesMarkup(data);
+    renderArticlesMarkup(data);
     Loading.remove();
   } catch (error) {
     Loading.remove();
     console.log(error);
   }
-}
+};
 
-function appendArticlesMarkup(articles) {
-  galleryListEl.innerHTML = articlesTpl(articles)
-}
-handleQueryApi()
+const renderArticlesMarkup = articles => {
+  galleryListEl.innerHTML = articlesTpl(articles);
+};
+
+handleQueryApi();
+
+const paginationBtnHandle = async e => {
+  const textContentClickedBtn = e.target.textContent;
+
+  if (!isNaN(textContentClickedBtn)) {
+    apiService.changePage(textContentClickedBtn);
+  }
+  if (textContentClickedBtn === '-') {
+    apiService.decrementPage();
+  }
+  if (textContentClickedBtn === '+') {
+    apiService.incrementPage();
+  }
+  handleQueryApi();
+};
+
+paginationBtnContainerEl.addEventListener('click', paginationBtnHandle);
