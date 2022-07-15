7c8dfdcd591dc51472a18e37856880d10b0332fb
Viktoriia Nistorika <vnistorika@gmail.com>
2022-07-15 17:50:56 +0300

    upd

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -12,7 +12,7 @@ class ApiService {
    async getGenres (){
      const response = await axios.get (`https://api.themoviedb.org/3/genre/movie/list?api_key=${KEY}&language=en-US`)
     
-      console.log(response.data.genres);
+      // console.log(response.data.genres);
       return response.data.genres
    }
    
@@ -24,6 +24,26 @@ class ApiService {
      response.data.results.forEach(element => 
       element.release_date =element.release_date.slice(0, 4) 
     );
+    
+   
+
+    response.data.results.forEach(element=>
+      element.genre_ids.forEach(id=>
+        this.getGenres().then((response)=>{
+          return response.forEach((el)=>{
+                   if (id === el.id){
+                   
+                    element.genre_ids.push(el.name)
+                    
+                  }
+                  element.genre_ids.map((el,idx)=>{
+                    if(typeof el === 'number'){
+                      element.genre_ids.splice(idx, 1)
+                    }
+                  })
+              });
+              
+            })))
 
     console.log(response.data.results);
     return response.data.results;
