bc3a181f0b5f24f99461bbe895aa7f7b69f0e18c
AlisaLevchenko <good.luck.odria@gmail.com>
2022-07-19 10:15:57 +0300

    al-pagination_fix

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -10,12 +10,16 @@ class ApiService {
   }
 
   async getArticlesBySearch() {
-    axios.defaults.params = {
-      api_key: KEY,
-      query: this.query,
-      page: this.page,
-    };
-    return await axios(`${URL}search/movie`).then(response => response.data);
+    // axios.defaults.params = {
+
+    // };
+    return await axios(`${URL}search/movie`, {
+      params: {
+        api_key: KEY,
+        query: this.query,
+        page: this.page,
+      },
+    }).then(response => response.data);
   }
 
   async getGenres() {
@@ -27,12 +31,16 @@ class ApiService {
   }
 
   async getTrendingArticles() {
-    const response = await axios.get(`${URL}/trending/movie/week?api_key=${KEY}&page=${this.page}`);
+    const response = await axios.get(
+      `${URL}/trending/movie/week?api_key=${KEY}&page=${this.page}`
+    );
 
     return response.data;
   }
   async getFullInfoById(id) {
-    const response = await axios.get(`${URL}movie/${id}?api_key=${KEY}&language=en-US`);
+    const response = await axios.get(
+      `${URL}movie/${id}?api_key=${KEY}&language=en-US`
+    );
 
     return response.data;
   }
