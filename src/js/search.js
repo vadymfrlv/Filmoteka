04bfda416429285455540ae61b352294f4f02e65
AlisaLevchenko <good.luck.odria@gmail.com>
2022-07-19 10:11:54 +0300

    al-pagination_serch

--- src/js/search.js
+++ src/js/search.js
@@ -1,4 +1,5 @@
 import ApiService from './api-service';
+import Pagination from 'tui-pagination';
 import { refs } from './refs';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import NormalizeDataApi from './normalize-data-api';
@@ -10,7 +11,28 @@ const renderTrending = new reHandleQueryTrendingApifs();
 const normalizeDataApi = new NormalizeDataApi();
 const renderGallery = new RenderGallery();
 
+const container = document.getElementById('tui-pagination-container');
+
 refs.form.addEventListener('submit', onSearch);
+
+const paginationSerch = new Pagination(container, {
+  itemsPerPage: 20,
+  visiblePages: 5,
+  centerAlign: true,
+  template: {
+    currentPage: '<a class="page-btn is-selected">{{page}}</a>',
+    page: '<a class="page-btn">{{page}}</a>',
+    moveButton: `<button class="move-btn move-btn-{{type}}"></button>`,
+    disabledMoveButton:
+      '<button class="move-btn move-btn-{{type}} disabled" disabled></button>',
+    moreButton: '<a class="page-btn next-is-ellip last-child">...</a>',
+  },
+});
+
+paginationSerch.on('afterMove', event => {
+  const currentPage = event.page;
+  onSearchPerPage(currentPage);
+});
 
 async function onSearch(e) {
   e.preventDefault();
@@ -33,14 +55,38 @@ async function onSearch(e) {
       return;
     }
     const genres = await apiService.getGenres();
-    const normalizedData = await normalizeDataApi.updateDataGenre(data, genres);
+    const normalizedData = await normalizeDataApi.updateDataTranding(
+      data,
+      genres
+    );
     renderGallery.renderGalleryMarkup(normalizedData);
     refs.formInput.value = '';
     Loading.remove();
-
+    paginationSerch.reset(data.total_results);
     return data;
   } catch (error) {
     Loading.remove();
     console.log(error);
   }
+}
+
+async function onSearchPerPage(page) {
+  apiService.page = page;
+  const data = await apiService.getArticlesBySearch();
+  if (data.results.length === 0) {
+    refs.errWarning.classList.remove('visually-hidden');
+    renderTrending.handleQueryTrendingDataApi();
+    return;
+  }
+  const genres = await apiService.getGenres();
+  const normalizedData = await normalizeDataApi.updateDataTranding(
+    data,
+    genres
+  );
+  renderGallery.renderGalleryMarkup(normalizedData);
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
 }
