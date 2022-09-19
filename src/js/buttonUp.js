f2beceadba429a035ff17ec05ec608281e791d07
Vadym Frolov <vdfroloff@gmail.com>
2022-09-19 17:32:05 +0300

    Update buttonUp.js

--- src/js/buttonUp.js
+++ src/js/buttonUp.js
@@ -29,3 +29,15 @@
 // });
 
 // backToTopButton.addEventListener('click', goToTop);
+
+import { refs } from './refs';
+window.onscroll = () => {
+  if (window.scrollY > 700) {
+    refs.btnGoTop.classList.remove('is-hidden');
+  } else {
+    refs.btnGoTop.classList.add('is-hidden');
+  }
+};
+refs.btnGoTop.onclick = () => {
+  window.scrollTo(0, 0);
+};
